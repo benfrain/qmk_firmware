77b1e39448595ff334064ff7c5d16d482b9cc09c
Ben Frain <bf@benfrain.com>
2022-01-23 22:08:52 +0000

    tweaked the encoder key

--- keyboards/sofle/keymaps/ben/config.h
+++ keyboards/sofle/keymaps/ben/config.h
@@ -1,3 +1,4 @@
 #pragma once
 
+#define ONESHOT_TAP_TOGGLE 5
 #define AUTO_SHIFT_TIMEOUT 150
--- keyboards/sofle/keymaps/ben/keymap.c
+++ keyboards/sofle/keymaps/ben/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_RBRC,
   KC_DEL,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
   SFT_T(KC_QUOT),  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MPLY,      KC_F12,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 TO(1),KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    OSL(1),KC_LCTL, LALT_T(KC_MINS), LGUI_T(KC_GRV), KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
@@ -157,7 +157,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         if (clockwise) {
             tap_code16(G(KC_MINS));
         } else {
-            tap_code16(G(KC_PLUS));
+            tap_code16(G(KC_EQL));
         }
     }
     return true;
