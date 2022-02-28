2efc1d04c08bf94cf0e71275e18b65b5f4c353f0
Ben Frain <bf@benfrain.com>
2022-02-28 08:19:03 +0000

    adding sofle boardwalk and wip dactyl 6x5_3

--- keyboards/boardwalk/keymaps/Ben/keymap.c
+++ keyboards/boardwalk/keymaps/Ben/keymap.c
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_ortho_5x14(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F11,  KC_F12,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
+HYPR_T(KC_ESC),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F11,  KC_F12,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_BSLS, KC_SLSH, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_RBRC,
         KC_DEL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    TD(CMD_MNS), TD(CMD_PLUS),KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_BSPC,
-        KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    RGB_TOG, RGB_M_R, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_GRV,  KC_RSPC,
-        TO(1),   KC_PGDN, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, HYPR(KC_NO)
+SFT_T(KC_QUOT), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    RGB_TOG, RGB_M_R, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_GRV,  KC_RSPC,
+        OSL(1),   KC_PGDN, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MPLY
     ),
 
     /* Function Layer
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_FN] = LAYOUT_ortho_5x14(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_NO,
+HYPR_T(KC_ESC),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_NO,
         _______, _______, KC_MS_U,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
---
+++ keyboards/handwired/dactyl_manuform/6x5_3/config.h
@@ -0,0 +1,33 @@
+// Serial, not i2c wiring
+#define USE_SERIAL
+
+// Default to master left
+#define MASTER_LEFT
+
+// Rows are doubled-up, because it is the total number of rows between the two halves.
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
+
+// wiring of each half, use the bottom left 6 and the bottom right 6 pins.
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+
+// Mirror the pins on the other side.
+// #define MATRIX_COL_PINS_RIGHT { B5, B4, E6, D7, C6, D4 }
+// #define MATRIX_ROW_PINS_RIGHT { F7, B1, B3, B2, B6 }
+
+// Default diode direction
+#define DIODE_DIRECTION COL2ROW
+
+// See https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
+// Turning this on would prevent a quick `'s` from resulting in `S` if the right quote key was a shift mod tap key.
+// However, it means I need to properly hold shift and the key to be shifted for longer than
+// the tapping term, which is worse. So this is disabled for now.
+// #define IGNORE_MOD_TAP_INTERRUPT
+// See explanation in `get_ignore_mod_tap_interrupt` of `keymap.c`
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
+
+// Tapping term is used for many things, e.g.
+// - how fast I need to double tap the number keys to get an F key
+// - for mod-shifts, how long they need to be held to be considered a hold rather than a tap.
+#define TAPPING_TERM 200
