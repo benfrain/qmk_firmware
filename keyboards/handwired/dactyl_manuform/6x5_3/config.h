9f796ca59509b1f1470da0b9f583c3f416b0ec3f
Ben Frain <bf@benfrain.com>
2022-03-02 13:09:52 +0000

    more work on trying to get a functional Dactyl

--- keyboards/handwired/dactyl_manuform/6x5_3/config.h
+++ keyboards/handwired/dactyl_manuform/6x5_3/config.h
@@ -1,31 +1,40 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define PRODUCT_ID      0x0000
+#define DEVICE_VER      0x0001
+#define PRODUCT         dactyl_bf
 // Serial, not i2c wiring
 #define USE_SERIAL
 
 // Default to master left
-#define MASTER_LEFT
-
+// #define MASTER_LEFT
+#define SPLIT_USB_DETECT
+#define SOFT_SERIAL_PIN D5
 // Rows are doubled-up, because it is the total number of rows between the two halves.
-#define MATRIX_ROWS 10
+#define MATRIX_ROWS 12
 #define MATRIX_COLS 6
 
-// wiring of each half, use the bottom left 6 and the bottom right 6 pins.
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6, F6 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4 }
 
-// Mirror the pins on the other side.
-// #define MATRIX_COL_PINS_RIGHT { B5, B4, E6, D7, C6, D4 }
-// #define MATRIX_ROW_PINS_RIGHT { F7, B1, B3, B2, B6 }
+#define DIODE_DIRECTION COL2ROW
 
 // Default diode direction
 #define DIODE_DIRECTION COL2ROW
 
-// See https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
-// Turning this on would prevent a quick `'s` from resulting in `S` if the right quote key was a shift mod tap key.
-// However, it means I need to properly hold shift and the key to be shifted for longer than
-// the tapping term, which is worse. So this is disabled for now.
-// #define IGNORE_MOD_TAP_INTERRUPT
-// See explanation in `get_ignore_mod_tap_interrupt` of `keymap.c`
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 
 // Tapping term is used for many things, e.g.
 // - how fast I need to double tap the number keys to get an F key
--- keyboards/handwired/dactyl_manuform/6x5_3/keymaps/ben/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x5_3/keymaps/ben/keymap.c
@@ -13,20 +13,23 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_COLEMAK] = LAYOUT_6x5_3(
+[_COLEMAK] = LAYOUT(
     HYPR_T(KC_ESC),     KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_LBRC,
     KC_TAB,             KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                                      KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_RBRC,
     KC_DEL,             KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                      KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_BSPC,
     SFT_T(KC_QUOT),     KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,                                      KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-    OSL(1),             KC_LCTL,LALT_T(KC_MINS),LGUI_T(KC_GRV),KC_SPC,KC_QUOT, KC_MPLY,   KC_F12,KC_EQL,KC_ENT,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT
+    OSL(1), KC_LCTL,      LALT_T(KC_MINS),LGUI_T(KC_GRV),KC_SPC,KC_QUOT,                              KC_F12,     KC_EQL,     KC_ENT,     KC_LEFT,    KC_DOWN,    KC_UP,
+    KC_SPC, KC_SPC
+
 ),
 
-[_RAISE] = LAYOUT_6x5_3(
+[_RAISE] = LAYOUT(
 HYPR_T(KC_ESC),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_NO,
   _______, _______, KC_MS_U,_______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_GRV, _______,                     _______,  _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______,  KC_MPLY, _______, _______, _______, _______, KC_BSLS, _______,
-  TO(0), _______, _______, _______, KC_MINS,_______,_______,_______,_______,       KC_EQL, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_BSLS, _______,
+  TO(0), _______, _______, _______, KC_MINS,_______,_______,       KC_EQL, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+  _______, _______
 )};
 
 #ifdef OLED_ENABLE
