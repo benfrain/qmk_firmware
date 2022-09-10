1c1ee803be9c84a6ccb733e69cad774505663258
Ben Frain <bf@benfrain.com>
2022-09-10 17:53:01 +0100

    Tweaks adding MEH and extra key double ups

--- keyboards/sofle/keymaps/ben/keymap.c
+++ keyboards/sofle/keymaps/ben/keymap.c
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT(
   HYPR_T(KC_ESC),   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_LBRC,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_RBRC,
+  MEH_T(KC_TAB),   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_RBRC,
   KC_DEL,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
-  SFT_T(KC_QUOT),  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MPLY,      KC_F12,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-    OSL(1),KC_LCTL, LALT_T(KC_MINS), LGUI_T(KC_GRV), KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+  SFT_T(KC_QUOT),  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MPLY,      KC_F12,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  SFT_T(KC_DQT),
+    OSL(1),LCTR_T(KC_EQL), LALT_T(KC_MINS), LGUI_T(KC_GRV), KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
