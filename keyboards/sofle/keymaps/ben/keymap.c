3110e909aff67f928508bef1de09b725fd936c16
Ben Frain <bf@benfrain.com>
2022-09-10 18:21:15 +0100

    Fix typo

--- keyboards/sofle/keymaps/ben/keymap.c
+++ keyboards/sofle/keymaps/ben/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MEH_T(KC_TAB),   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_RBRC,
   KC_DEL,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
   SFT_T(KC_QUOT),  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MPLY,      KC_F12,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  SFT_T(KC_DQT),
-    OSL(1),LCTR_T(KC_EQL), LALT_T(KC_MINS), LGUI_T(KC_GRV), KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    OSL(1),LCTL_T(KC_EQL), LALT_T(KC_MINS), LGUI_T(KC_GRV), KC_SPC,      KC_ENT,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
